70b2fd0cdc0a9e17fda0b43a20f440a89e087366
Busi Reddy <busisatya@gmail.com>
2021-12-07 07:19:31 -0500

    Time: 77 ms (41.52%), Space: 14.3 MB (97.28%) - LeetHub

--- permutations-ii/permutations-ii.py
+++ permutations-ii/permutations-ii.py
@@ -3,15 +3,18 @@ class Solution:
             if start == len(nums):
                 return [[]]
             ret = []
+            set_int = set()
             for i in range(start, len(nums)):
-                nums[start], nums[i] = nums[i], nums[start]
-                perms = self.helper(nums, start + 1)
-                for perm in perms:
-                    perm.append(nums[start])
-                ret.extend(perms)
-                nums[start], nums[i] = nums[i], nums[start]
+                if nums[i] not in set_int:
+                    nums[start], nums[i] = nums[i], nums[start]
+                    perms = self.helper(nums, start + 1)
+                    for perm in perms:
+                        perm.append(nums[start])
+                    ret.extend(perms)
+                    nums[start], nums[i] = nums[i], nums[start]
+                set_int.add(nums[i])
             return ret
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
         ans = self.helper(nums,0)
-        ans = set([tuple(perm) for perm in ans])
+        # ans = set([tuple(perm) for perm in ans])
         return list(ans)
