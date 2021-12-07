7bd25e15c48df25324a618311f4361cca8ecef55
Busi Reddy <busisatya@gmail.com>
2021-12-07 06:25:04 -0500

    Time: 57 ms (17.77%), Space: 14.5 MB (45.93%) - LeetHub

--- permutations/permutations.py
+++ permutations/permutations.py
@@ -1,4 +1,17 @@
-from itertools import permutations
+
 class Solution:
+    def helper(self,nums,start):
+        if start == len(nums):
+            return [[]]
+        ret = []
+        for i in range(start,len(nums)):
+            nums[start],nums[i] = nums[i],nums[start]
+            perms = self.helper(nums,start+1)
+            for perm in perms:
+                perm.append(nums[start])
+            ret.extend(perms)
+            nums[start],nums[i] = nums[i],nums[start]
+        return ret
     def permute(self, nums: List[int]) -> List[List[int]]:
-        return permutations(nums)
+        return self.helper(nums,0)
+        
