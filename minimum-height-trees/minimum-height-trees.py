c9d40edce5dd6378ece85d4d3feabcc613a93e9a
Busi Reddy <busisatya@gmail.com>
2021-12-16 07:43:01 -0500

    Time: 777 ms (6.23%), Space: 20.4 MB (6.72%) - LeetHub

---
+++ minimum-height-trees/minimum-height-trees.py
@@ -0,0 +1,16 @@
+import collections
+class Solution:
+    
+    def findMinHeightTrees(self, n, edges):
+        d = collections.defaultdict(set)
+        for u, v in edges:
+            d[u].add(v)
+            d[v].add(u)
+        s = set(range(n))
+        while len(s) > 2:
+            leaves = set(i for i in s if len(d[i]) == 1)
+            s -= leaves
+            for i in leaves:
+                for j in d[i]:
+                    d[j].remove(i)
+        return list(s)
