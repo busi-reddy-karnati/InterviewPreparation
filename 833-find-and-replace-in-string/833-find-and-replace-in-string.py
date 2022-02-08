571067fb9cc3bf22c4306344a62ae9f899ca8a00
Busi Reddy <busisatya@gmail.com>
2022-02-08 01:10:35 -0500

    Time: 66 ms (23.97%), Space: 13.9 MB (93.05%) - LeetHub

---
+++ 833-find-and-replace-in-string/833-find-and-replace-in-string.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findReplaceString(self, s: str, indices: List[int], sources: List[str], targets: List[str]) -> str:
+        for index,source,target in sorted(zip(indices,sources,targets),reverse=True):
+            if s[index:].startswith(source):
+                s = s[:index] + target + s[index+len(source):]
+        return s
+        
