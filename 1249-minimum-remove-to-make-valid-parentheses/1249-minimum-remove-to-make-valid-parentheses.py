e51d81646ff3203905fdf9e09468a57066794f3d
Busi Reddy <busisatya@gmail.com>
2022-03-10 17:19:50 -0500

    Time: 109 ms (85.11%), Space: 15.9 MB (41.44%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
@@ -1,23 +1,16 @@
-from collections import deque
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
-        stack = deque()
-        for i,char in enumerate(list(s)):
-            if char == '(':
+        stack = []
+        l = list(s)
+        for i,char in enumerate(s):
+            if s[i] == '(':
                 stack.append(i)
-            elif char == ')':
-                if not stack or s[stack[-1]] == ')':
-                    # print(i)
-                    stack.append(i)
+            elif s[i] == ')':
+                if not stack:
+                    l[i] = ""
                 else:
-                    stack.pop()
-        ans = []
-        ind_stack = 0
-        # print(stack)
-        for i in range(len(s)):
-            if ind_stack < len(stack) and stack[ind_stack] == i:
-                ind_stack += 1
-            else:
-                ans.append(s[i])
-        return "".join(ans)
+                    stack.pop(-1)
         
+        for ind in stack:
+            l[ind] = ""
+        return "".join(l)
