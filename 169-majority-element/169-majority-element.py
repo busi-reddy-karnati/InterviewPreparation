0f80742aa69281483981ac68aa9de8873dce261d
Busi Reddy <busisatya@gmail.com>
2022-04-08 21:30:56 -0400

    Time: 203 ms (63.20%), Space: 15.6 MB (30.27%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -1,13 +1,16 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
+        maj_elem = nums[0]
         freq = 1
-        nummax = nums[0]
-        for num in nums:
-            if num == nummax:
-                freq+=1
-            else:
-                if freq == 1:
-                    nummax = num
+        for i in range(1,len(nums)):
+            if freq == 1:
+                if maj_elem != nums[i]:
+                    maj_elem = nums[i]
                 else:
+                    freq = 2
+            else:
+                if maj_elem != nums[i]:
                     freq -= 1
-        return nummax
+                else:
+                    freq += 1
+        return maj_elem
