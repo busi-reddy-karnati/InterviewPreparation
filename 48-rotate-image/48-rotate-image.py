1440a95c3681668adf33af3c362fedd0508221b9
Busi Reddy <busisatya@gmail.com>
2022-05-24 21:57:10 -0400

    Time: 50 ms (45.63%), Space: 13.9 MB (74.71%) - LeetHub

--- 48-rotate-image/48-rotate-image.py
+++ 48-rotate-image/48-rotate-image.py
@@ -1,36 +1,12 @@
 class Solution:
-    def rotate(self, a: List[List[int]]) -> None:
-        '''
-        Questions: 
-        Is this a square matrix?
-        What are the constrains?
-        possible range of values in the matrix?
-        
-        brute force:
-        start going by column by column and inserting into the new matrix
-        complexity: time:n^2
-                    space:n^2
-        
-        better?
-        do a transpose and a mirror image(inplace possible)
-        complexity: time:n^2
-                    space: 1
-        
-        '''
-        #Transpose
-        n = len(a)
+    def rotate(self, matrix: List[List[int]]) -> None:
+        n = len(matrix)
+        # Transpose
         for i in range(n):
-            for j in range(i):
-                a[i][j], a[j][i] = a[j][i],a[i][j]
-        
-        #done transpose - working
-            
-        
-        #mirror
+            for j in range(0,i):
+                matrix[i][j],matrix[j][i] = matrix[j][i],matrix[i][j]
+        # print(matrix) success transpose
         for i in range(n):
-            for j in range(n//2):
-                ind = j
-                swap_ind = n-j-1
-                a[i][ind],a[i][swap_ind] = a[i][swap_ind],a[i][ind]
-            
+            for j in range(int(n/2)):
+                matrix[i][j], matrix[i][n-j-1] = matrix[i][n-j-1],matrix[i][j]
         
