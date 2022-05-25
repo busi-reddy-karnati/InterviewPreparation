0a12652ecafc02ca71730e306a38a17cbe4e3069
Busi Reddy <busisatya@gmail.com>
2022-05-24 20:44:40 -0400

    Time: 1042 ms (59.56%), Space: 19.3 MB (6.70%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,15 +1,34 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        index_map = {}
-        for i in range(len(nums)):
-            index_map[nums[i]] = i
+        p = []
+        n = []
+        zs = 0
+        for num in nums:
+            if num < 0 :
+                n.append(num)
+            elif num > 0:
+                p.append(num)
+            else:
+                zs += 1
         ans = set()
-        for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                num1 = nums[i]
-                num2 = nums[j]
-                num3 = -(nums[i]+nums[j])
-                if num3 in index_map and index_map[num3] not in {i,j}:
-                    l = tuple(sorted((num1, num2, num3)))
-                    ans.add(l)
+        if zs > 2:
+            ans.add((0,0,0))
+        ps = set(p)
+        ns = set(n)
+        if zs > 0:
+            for num in ps:
+                if -num in ns:
+                    ans.add((-num,0,num))
+        for i in range(len(p)):
+            for j in range(i+1, len(p)):
+                target = -(p[i]+p[j])
+                if target in ns:
+                    ans.add(tuple(sorted((p[i],p[j],target))))
+        for i in range(len(n)):
+            for j in range(i+1, len(n)):
+                target = -(n[i]+n[j])
+                if target in ps:
+                    ans.add(tuple(sorted((n[i],n[j],target))))
         return [list(i) for i in ans]
+    
+                
