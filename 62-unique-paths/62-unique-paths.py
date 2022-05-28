3bc8d2628eb273d401700303ed4f5a3e532ae003
Busi Reddy <busisatya@gmail.com>
2022-05-27 21:20:14 -0400

    Time: 53 ms (26.90%), Space: 13.9 MB (74.28%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -9,11 +9,10 @@ class Solution:
         
 #         #Going to m,n can be from (m-1,n) or (m,n-1)
 #         return self.uniquePaths(m-1,n)+self.uniquePaths(m,n-1)
-        downs = m-1
-        rights = n-1
+        
         ans = 1
-        init = downs+rights
-        mini = min(downs,rights)
+        init = n+m-2
+        mini = min(n,m)-1
         for i in range(mini):
             ans = ans*(init-i)
         
