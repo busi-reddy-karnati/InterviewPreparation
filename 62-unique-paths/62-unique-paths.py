449aa6e6591b86018f9661e03ba73afab84f6bdb
Busi Reddy <busisatya@gmail.com>
2022-05-27 21:16:13 -0400

    Time: 68 ms (7.12%), Space: 13.9 MB (74.28%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,7 +1,18 @@
-import math
-def ncr(n,r):
-    return math.factorial(n)/(math.factorial(n-r)*math.factorial(r))
+def factorial(n):
+    ans = 1
+    for i in range(1,n+1):
+        ans *= i
+    return ans
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        return int(ncr(m+n-2,n-1))
-    
+        #dp solution
+#         if m < 1 or n < 1:
+#             return 0
+#         if m == 1 or n == 1:
+#             return 1
+        
+#         #Going to m,n can be from (m-1,n) or (m,n-1)
+#         return self.uniquePaths(m-1,n)+self.uniquePaths(m,n-1)
+        downs = m-1
+        rights = n-1
+        return int((factorial(downs+rights)/((factorial(downs))*(factorial(rights)))))
