05253f919cc844f76501b00549f2939563e82bfc
Busi Reddy <busisatya@gmail.com>
2022-05-30 21:26:33 -0400

    Time: 200 ms (32.39%), Space: 14.8 MB (53.19%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.py
@@ -1,41 +1,32 @@
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
-        '''
-        BF: have a hashtable with rows and cols as entries
-        if an element is 0, makes its corresponding row and col as true
-        for each element, see if the corresponding row or col are true, if so set 0. Else 1
-        
-        '''
-        rows = len(matrix)
-        cols = len(matrix[0])
-        zz = matrix[0][0] == 0
-        zr = False
-        zc = False
-        for i in range(cols):
-            if matrix[0][i] == 0:
-                zr = True
-        for i in range(rows):
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        zr = 1
+        zc = 1
+        for i in range(len(matrix)):
             if matrix[i][0] == 0:
-                zc = True
-        for row in range(1,rows):
-            for col in range(1,cols):
-                if matrix[row][col] == 0:
-                    matrix[0][col] = 0
-                    matrix[row][0] = 0
-        for row in range(1,rows):
-            for col in range(1,cols):
-                if matrix[0][col] == 0 or matrix[row][0]==0:
-                    matrix[row][col] = 0
-        if zz:
-            for row in range(rows):
-                matrix[row][0] = 0
-            for col in range(cols):
-                matrix[0][col] = 0
-        else:
-            if zr:
-                for i in range(cols):
-                    matrix[0][i] = 0
-            if zc:
-                for i in range(rows):
+                zr = 0
+        for i in range(len(matrix[0])):
+            if matrix[0][i] == 0:
+                zc = 0
+        for i in range(1,len(matrix)):
+            for j in range(1,len(matrix[0])):
+                if matrix[i][j] == 0:
+                    matrix[0][j] = 0
                     matrix[i][0] = 0
-                    
+        for i in range(1,len(matrix)):
+            for j in range(1,len(matrix[0])):
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
+                    matrix[i][j] = 0
+        if zr == 0:
+            for i in range(len(matrix)):
+                matrix[i][0] = 0
+        if zc == 0:
+            for i in range(len(matrix[0])):
+                matrix[0][i] = 0
+        
+            
+                
+        
