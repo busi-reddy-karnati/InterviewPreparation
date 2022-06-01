7988439eb05abf9ab1de2565555b420cf1bec98b
Busi Reddy <busisatya@gmail.com>
2022-05-31 23:50:05 -0400

    Time: 54 ms (77.54%), Space: 18.1 MB (42.18%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.py
@@ -4,19 +4,16 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-
 class Solution:
-    num_nodes_seen = 0
-    ans = 0
-    def helper(self,root,k):
+    inorder = []
+    def inorderTraversal(self,root):
         if not root:
             return
-        self.helper(root.left,k)
-        self.num_nodes_seen += 1
-        if self.num_nodes_seen == k:
-            self.ans = root.val
-        self.helper(root.right,k)
+        self.inorderTraversal(root.left)
+        self.inorder.append(root.val)
+        self.inorderTraversal(root.right)
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        self.num_nodes_seen = 0
-        self.helper(root,k)
-        return self.ans
+        self.inorder = []
+        self.inorderTraversal(root)
+        return self.inorder[k-1]
+        
