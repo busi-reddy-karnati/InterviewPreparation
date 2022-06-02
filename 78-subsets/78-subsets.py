23b760ba219094c067c5b52b5dbf3338b0b64771
Busi Reddy <busisatya@gmail.com>
2022-06-02 00:34:06 -0400

    Time: 47 ms (51.45%), Space: 14.1 MB (33.41%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,15 +1,17 @@
 class Solution:
+    subsets_set = []
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        self.ans = []
-        subset = []
-        self.helper(nums,0,subset)
-        return self.ans
-    def helper(self,nums,index,subset):
-        if index == len(nums):
-            self.ans.append([num for num in subset])
+        self.subsets_set = []
+        self.all_permutations(nums,0,[])
+        return (self.subsets_set)
+    
+    def all_permutations(self,nums,index, temp_list):
+        
+        if index >= len(nums):
+            self.subsets_set.append([i for i in temp_list])
             return
-        subset.append(nums[index])
-        self.helper(nums,index+1,subset)
-        subset.pop(-1)
-        self.helper(nums,index+1,subset)
+        self.all_permutations(nums,index+1,temp_list)
+        temp_list.append(nums[index])
+        self.all_permutations(nums,index+1,temp_list)
+        temp_list.pop()
         
