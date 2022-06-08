20fd5a4ec0ce707edb6a87471818e5bf379fda4a
Busi Reddy <busisatya@gmail.com>
2022-06-08 16:22:30 -0400

    Time: 119 ms (23.65%), Space: 14 MB (10.01%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -3,40 +3,47 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-def size(head):
+def getLen(head):
     temp = head
-    n =  0
+    ans = 0
     while temp:
-        n+=1
+        ans += 1
         temp = temp.next
-    return n
+    return ans
+def add(head,n):
+    temp = head
+    while temp.next:
+        temp = temp.next
+    for i in range(n):
+        newNode = ListNode(0)
+        temp.next = newNode
+        temp = temp.next
+    
+        
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        n1 = size(l1)
-        n2 = size(l2)
-        if n1<n2:
-            temp = l1
+        n1 = getLen(l1)
+        n2 = getLen(l2)
+        if n1>n2:
+            add(l2,n1-n2)
         else:
-            temp = l2
-        
-        while temp.next:
-            temp = temp.next
-        for i in range(abs(n1-n2)):
-            temp.next = ListNode(0)
-            temp = temp.next
+            add(l1,n2-n1)
         carry = 0
-        ans = l1
-        while l1.next:
-            sum = l1.val+l2.val+carry
+        sum = 0
+        ptr1 = l1
+        ptr2 = l2
+        
+        while ptr1.next:
+            sum = ptr1.val+ptr2.val+carry
             carry = sum//10
             sum = sum%10
-            l1.val = sum
-            l1 = l1.next
-            l2 = l2.next
-        sum = l1.val+l2.val+carry
+            ptr1.val = sum
+            ptr1 = ptr1.next
+            ptr2 = ptr2.next
+        sum = ptr1.val+ptr2.val+carry
         carry = sum//10
-        sum%=10
-        l1.val = sum
+        sum = sum%10
+        ptr1.val = sum
         if carry > 0:
-            l1.next = ListNode(carry)
-        return ans
+            ptr1.next = ListNode(carry)
+        return l1
