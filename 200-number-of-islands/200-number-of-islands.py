e56b73c4671f27517741bf14e193f6523dabe974
Busi Reddy <busisatya@gmail.com>
2022-06-08 21:05:23 -0400

    Time: 736 ms (5.09%), Space: 16.4 MB (80.25%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,15 +1,14 @@
-def clear(grid,row,col):
-    if row == len(grid) or col == len(grid[0]) or col == -1 or row == -1:
-        return
-    if grid[row][col] == "0":
-        return
-    grid[row][col] = "0"
-    clear(grid,row,col+1)
-    clear(grid,row+1,col)
-    clear(grid,row-1,col)
-    clear(grid,row,col-1)
-    
 class Solution:
+    def clear(self, grid, row, col):
+        if row < 0 or row >= len(grid) or col < 0 or col >= len(grid[0]):
+            return
+        if grid[row][col] == "1":
+            grid[row][col] = "0"
+            self.clear(grid,row+1,col)            
+            self.clear(grid,row-1,col)
+            self.clear(grid,row,col+1)
+            self.clear(grid,row,col-1)
+
     def numIslands(self, grid: List[List[str]]) -> int:
         rows = len(grid)
         cols = len(grid[0])
@@ -17,7 +16,6 @@ class Solution:
         for row in range(rows):
             for col in range(cols):
                 if grid[row][col] == "1":
-                    ans += 1
-                    clear(grid,row,col)
+                    self.clear(grid, row, col)
+                    ans += 1                
         return ans
-    
