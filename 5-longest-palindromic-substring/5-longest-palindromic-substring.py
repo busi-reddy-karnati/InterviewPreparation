2803e2deae0e6c6aafc4d82137624c02d39ef659
Busi Reddy <busisatya@gmail.com>
2022-06-16 00:27:21 -0400

    Time: 1845 ms (34.94%), Space: 15.2 MB (13.39%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -8,16 +8,19 @@ class Solution:
         ans_len = 0
         ans_string = ""
         n = len(s)
+        
+        #idhi odd case. manam anukunnam ga keeping one letter as center
         for i in range(n):
             offset = 0
             while i-offset >=0 and i+offset<n:
                 if s[i-offset] != s[i+offset]:
                     break
+                #2*offset enduku ante, pointers atu itu vellina length ki 2 times cheste na ga motham length ochedhi plus madhyalo oka letter
                 if ((2*offset)+1) > ans_len:
                     ans_len = ((2*offset)+1)
                     ans_string = s[i-offset:i+offset+1]
                 offset += 1
-
+        #Idhi Even case
         for i in range(n-1):
             offset = 0
             while i-offset >= 0 and i+offset+1<n:
