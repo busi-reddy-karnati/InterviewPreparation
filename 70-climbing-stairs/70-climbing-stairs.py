2ea5c67f95fd39e499a25794c3d1e9863a200d13
Busi Reddy <busisatya@gmail.com>
2022-06-29 01:25:38 -0400

    Time: 60 ms (13.04%), Space: 13.8 MB (95.99%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,12 +1,14 @@
 class Solution:
-    global_dp = {}
     def climbStairs(self, n: int) -> int:
-        if n in self.global_dp:
-            return self.global_dp[n]   
+        self.dp = {}
+        return self.helper(n)
+    def helper(self, n):
+        if n in self.dp:
+            return self.dp[n]
         if n == 1:
             return 1
         if n == 2:
             return 2
-        self.global_dp[n] = self.climbStairs(n-1)+self.climbStairs(n-2)
-        return self.global_dp[n]
-    
+        ans = self.helper(n-1)+self.helper(n-2)
+        self.dp[n] = ans
+        return ans
