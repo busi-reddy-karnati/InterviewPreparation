396d63407d091d455cb8605cc8b6c646b1ff10e1
Busi Reddy <busisatya@gmail.com>
2022-08-22 21:02:17 -0400

    Time: 213 ms (55.28%), Space: 16.8 MB (50.32%) - LeetHub

--- 136-single-number/136-single-number.py
+++ 136-single-number/136-single-number.py
@@ -1,6 +1,6 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        ans = 0
-        for num in nums:
-            ans = ans^num
+        ans = nums[0]
+        for i in range(1,len(nums)):
+            ans = ans ^ nums[i]
         return ans
