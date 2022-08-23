1d97a07521ed00d4103912c60c23258f5686a767
Busi Reddy <busisatya@gmail.com>
2022-08-23 16:24:29 -0400

    Time: 233 ms (11.29%), Space: 19 MB (33.51%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,15 +1,16 @@
-def giveSortedString(s):
-    s = list(s)
-    s.sort()
-    return "".join(s)
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        mapOfAnagrams = {}
+        def string_map(s):
+            li = [0 for i in range(26)] #For each char
+            for char in s:
+                li[ord(char)-ord('a')] += 1
+            tu = tuple(li)
+            return tu
+        hmap = {}
         for string in strs:
-            modifiedString = giveSortedString(string)
-            if modifiedString in mapOfAnagrams:
-                mapOfAnagrams[modifiedString].append(string)
+            str_tu = string_map(string)
+            if str_tu in hmap:
+                hmap[str_tu].append(string)
             else:
-                mapOfAnagrams[modifiedString] = [string]
-        return mapOfAnagrams.values()
-        
+                hmap[str_tu] = [string]
+        return list(hmap.values())
