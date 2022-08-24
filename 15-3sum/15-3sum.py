4a24c938c9da34ad083c7a7064ddbe815c9d48b2
Busi Reddy <busisatya@gmail.com>
2022-08-24 14:38:03 -0400

    Time: 1825 ms (34.08%), Space: 18.1 MB (72.13%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,34 +1,22 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        p = []
-        n = []
-        zs = 0
-        for num in nums:
-            if num < 0 :
-                n.append(num)
-            elif num > 0:
-                p.append(num)
-            else:
-                zs += 1
-        ans = set()
-        if zs > 2:
-            ans.add((0,0,0))
-        ps = set(p)
-        ns = set(n)
-        if zs > 0:
-            for num in ps:
-                if -num in ns:
-                    ans.add((-num,0,num))
-        for i in range(len(p)):
-            for j in range(i+1, len(p)):
-                target = -(p[i]+p[j])
-                if target in ns:
-                    ans.add(tuple(sorted((p[i],p[j],target))))
-        for i in range(len(n)):
-            for j in range(i+1, len(n)):
-                target = -(n[i]+n[j])
-                if target in ps:
-                    ans.add(tuple(sorted((n[i],n[j],target))))
-        return [list(i) for i in ans]
-    
-                
+        nums.sort()
+        ans = []
+        for i in range(len(nums)):
+            if i > 0 and nums[i]==nums[i-1]:
+                continue
+            low = i+1
+            high = len(nums)-1
+            while (high > low):
+                sum = nums[high]+nums[low]+nums[i]
+                if sum == 0:
+                    ans.append([nums[high], nums[low], nums[i]])
+                    #Don't use the same number again
+                    while nums[low] == nums[low+1] and low+1<high:
+                        low = low + 1
+                    low = low + 1
+                elif sum > 0:
+                    high -= 1
+                else:
+                    low += 1
+        return ans
