05d51843b00047a6a0a98abcce43611956d2f4a1
Busi Reddy <busisatya@gmail.com>
2022-08-30 18:49:02 -0400

    Time: 2387 ms (5.00%), Space: 36.4 MB (41.55%) - LeetHub

---
+++ 853-car-fleet/853-car-fleet.py
@@ -0,0 +1,14 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        pos_vel = sorted(zip(position,speed), reverse=True)
+        max_seen = float("-inf")
+        ans = 0
+        for elem in pos_vel:
+            vel = elem[1]
+            pos = elem[0]
+            time = (target-pos)/vel
+            if max_seen < time:#I reach after everyone on my right, so i make one fleet
+                ans += 1
+                max_seen = time
+        return ans
+        
