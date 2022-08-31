3660f26b5be15b318064a0d7d6871ba41f5d36c8
Busi Reddy <busisatya@gmail.com>
2022-08-31 15:08:23 -0400

    Time: 91 ms (70.19%), Space: 14 MB (10.10%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -3,47 +3,41 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-def getLen(head):
-    temp = head
-    ans = 0
-    while temp:
-        ans += 1
-        temp = temp.next
-    return ans
-def add(head,n):
-    temp = head
-    while temp.next:
-        temp = temp.next
-    for i in range(n):
-        newNode = ListNode(0)
-        temp.next = newNode
-        temp = temp.next
-    
-        
 class Solution:
+    def length(self, head):
+        temp = head
+        ans = 0
+        while temp:
+            ans += 1
+            temp = temp.next
+        return ans
+    def add_dummy_nodes(self, head, n):
+        temp = head
+        while temp.next:
+            temp = temp.next
+        for i in range(n):
+            temp.next = ListNode(0)
+            temp = temp.next
+        return head
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        n1 = getLen(l1)
-        n2 = getLen(l2)
-        if n1>n2:
-            add(l2,n1-n2)
+        n1 = self.length(l1) 
+        n2 = self.length(l2)
+        if n1 < n2:
+            self.add_dummy_nodes(l1, abs(n1-n2))
         else:
-            add(l1,n2-n1)
+            self.add_dummy_nodes(l2, abs(n1-n2))
+        ans = l1
         carry = 0
-        sum = 0
-        ptr1 = l1
-        ptr2 = l2
+        tail = l1
+        while l1 and l2:
+            sum_of_nodes = l1.val+l2.val+carry
+            carry = sum_of_nodes // 10
+            sum_of_nodes = sum_of_nodes%10
+            l1.val = sum_of_nodes    
+            tail = l1
+            l1 = l1.next
+            l2 = l2.next
+        if carry:
+            tail.next = ListNode(carry)
+        return ans
         
-        while ptr1.next:
-            sum = ptr1.val+ptr2.val+carry
-            carry = sum//10
-            sum = sum%10
-            ptr1.val = sum
-            ptr1 = ptr1.next
-            ptr2 = ptr2.next
-        sum = ptr1.val+ptr2.val+carry
-        carry = sum//10
-        sum = sum%10
-        ptr1.val = sum
-        if carry > 0:
-            ptr1.next = ListNode(carry)
-        return l1
