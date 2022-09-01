9b3bacf63c5ef91b70d643a4315ebf9f8276f64c
Busi Reddy <busisatya@gmail.com>
2022-09-01 12:22:35 -0400

    Time: 1225 ms (22.38%), Space: 27.9 MB (56.92%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.py
@@ -1,29 +1,6 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        '''
-        Brute force: keep a hashmap and keep checking each one,
-        if you see that there is an element more than once, then return
-        Time: N
-        Space: N
-        
-        Better: sort and see if each one is equal to beside
-        Time: NlogN
-        Space: N
-        
-        Better2: sum(array)-sum(set(array))
-        Time: N
-        space: N
-        
-        
-        Best:
-        for each number, index = abs(number)-1
-        make that index as -(num at that index)
-        if already negative then, return        
-        
-        '''
-        for i in range(len(nums)):
-            index = abs(nums[i])-1
-            if nums[index] < 0:
-                return index+1
-            nums[index] = -nums[index]
-        
+        nums.sort()
+        for i in range(len(nums)-1):
+            if nums[i] == nums[i+1]:
+                return nums[i]
