45e0137b15f45291433702552e29f828fe3c8188
Busi Reddy <busisatya@gmail.com>
2022-09-01 22:00:29 -0400

    Time: 1340 ms (25.32%), Space: 27.8 MB (78.09%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -1,16 +1,14 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        #1. Find all subarrays and see which has the highest sum. O(n2), O(n)
-        #2. Using sliding window, Find maximum subarray in all sliding windows O(n2), O(1)
-        #3. Use Kadanes Algorithm. Either we take an element to be appended to the previous subarray or start fresh from here O(n), O(1)
-        #4 Dp O(n), O(n)
-        dp = []
-        dp.append(nums[0])
-        ans = nums[0]
+        if not nums:
+            return 0
+        max_sum = nums[0]
+        sub_array_sum = nums[0]
         for i in range(1,len(nums)):
-            temp = nums[i]
-            if dp[-1] > 0:
-                temp += dp[-1]
-            dp.append(temp)
-            ans = max(ans,temp)
-        return ans
+            num = nums[i]
+            if sub_array_sum + num > num:
+                sub_array_sum = sub_array_sum + num
+            else:
+                sub_array_sum = num
+            max_sum = max(max_sum, sub_array_sum)
+        return max_sum
