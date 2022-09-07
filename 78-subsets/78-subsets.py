328765f87a9b455de28a6f4f0412edb10945e223
Busi Reddy <busisatya@gmail.com>
2022-09-07 01:48:01 -0400

    Time: 65 ms (21.93%), Space: 14.2 MB (35.94%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,16 +1,17 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         subsets = []
-        def helper(ind, temp):
+        temp = []
+        def helper(ind):
             nonlocal subsets
             if ind == len(nums):
-                subsets.append([num for num in temp])
+                subsets.append(temp.copy())
                 return
-            helper(ind+1, temp)
+            helper(ind+1)
             temp.append(nums[ind])
-            helper(ind+1, temp)
+            helper(ind+1)
             temp.pop()
         
-        helper(0, [])
+        helper(0)
         return subsets
         
