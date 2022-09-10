5b1e90bc23dfa3ed3712704639f66eb31da218d8
Busi Reddy <busisatya@gmail.com>
2022-09-09 23:35:12 -0400

    Time: 25 ms (99.38%), Space: 14.1 MB (19.72%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,13 +1,15 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        ans_map = {}
         def dp(index):
+            
             nonlocal ans_map
-            if index in ans_map:
-                return ans_map[index]
             if index >= len(nums):
                 return 0
-            ans = max(dp(index+1), dp(index+2)+nums[index])
+            if index in ans_map:
+                return ans_map[index]
+            ans = max(nums[index]+dp(index+2), dp(index+1))
             ans_map[index] = ans
             return ans
-        ans_map = {}
         return dp(0)
+        
