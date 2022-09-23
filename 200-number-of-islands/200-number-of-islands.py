9102e4c24572a6a913ed3987a6c7ae8e8be327a0
Busi Reddy <busisatya@gmail.com>
2022-09-23 17:26:19 -0400

    Time: 958 ms (7.09%), Space: 22.2 MB (15.18%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,21 +1,25 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        def bfs(x,y):
-            nonlocal grid
-            if x >= len(grid) or x < 0 or y >= len(grid[0]) or y < 0:
+        
+        visited = set()
+        
+        def visit(i, j):
+            if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]):
                 return
-            if grid[x][y] == "0":
+            nonlocal visited
+            if grid[i][j] == "0" or (i,j) in visited:
                 return
-            directions = [[0,1],[1,0],[-1,0],[0,-1]]
-            grid[x][y] = "0"
+            
+            directions = [[-1,0],[0,-1],[0,1],[1,0]]
+            visited.add((i,j))
             for direction in directions:
-                x_new = x + direction[0]
-                y_new = y + direction[1]
-                bfs(x_new, y_new)
+                new_x = i + direction[0]
+                new_y = j + direction[1]
+                visit(new_x,new_y)
         ans = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j] == "1":
+                if grid[i][j] == "1" and (i,j) not in visited:
                     ans += 1
-                    bfs(i,j)
+                    visit(i,j)
         return ans
