ab108ebbfb1ab4fdf033c32efaf0469b44ab6358
Busi Reddy <busisatya@gmail.com>
2022-09-27 16:40:53 -0400

    Time: 692 ms (94.76%), Space: 136 MB (59.64%) - LeetHub

--- 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.py
+++ 2096-step-by-step-directions-from-a-binary-tree-node-to-another/2096-step-by-step-directions-from-a-binary-tree-node-to-another.py
@@ -1,23 +1,48 @@
-from collections import deque
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def getDirections(self, root: Optional[TreeNode], source: int, destination: int) -> str:
-        path_to_source = ""
-        path_to_destination = ""
-        stack = deque()
-        stack.append((root,""))
-        while stack and (not path_to_source or not path_to_destination):
-            node,path = stack.pop()
-            if node.val == source:
-                path_to_source = path
-            if node.val == destination:
-                path_to_destination = path
-            if node.left:
-                stack.append((node.left,path+"L"))
-            if node.right:
-                stack.append((node.right,path+"R"))
-        pointer = 0
-        min_len = min(len(path_to_source),len(path_to_destination))
-        while pointer < min_len and path_to_source[pointer] == path_to_destination[pointer]:
-            pointer += 1
-        ans = "U"*(len(path_to_source)-pointer) + path_to_destination[pointer:]
+    def getDirections(self, root: Optional[TreeNode], start: int, end: int) -> str:
+        #2 find the path from LCA to start
+            #2.1 reverse the path that is there
+            #If I generate L(Left), R(Right) Then i Can replace with that length of parents
+        #3 find path from LSA to end
+        def find_path(node, target):
+            nonlocal trace_path
+            if not node:
+                return False
+            if node.val == target:
+                return True
+            left = find_path(node.left, target)
+            if left:
+                trace_path.appendleft("L")
+                return True
+            right = find_path(node.right, target)
+            if right:
+                trace_path.appendleft("R")
+                return True
+        def find_lca(node):
+            if not node:
+                return
+            if node.val == start:
+                return node
+            if node.val == end:
+                return node
+            left = find_lca(node.left)
+            right = find_lca(node.right)
+            if left and right:
+                return node
+            return left or right
+        lca = find_lca(root)
+        trace_path = collections.deque()
+        find_path(lca, start)
+        ans = "U"*len(trace_path)
+        trace_path = collections.deque()
+        find_path(lca, end)
+        
+        ans = ans + "".join(trace_path)
         return ans
+        
